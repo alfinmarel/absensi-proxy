09d9eda09046993bb527ad43b24f823c1d84113d
alfinmarel <86781426+alfinmarel@users.noreply.github.com>
2025-07-06 07:45:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 export default async function handler(req, res) {
   const APPS_SCRIPT_BASE_URL = 'https://script.google.com/macros/s/AKfycbyq5JSyO7LBfCxbNxo97yN9i3MdT8TvHhZaT_NKBm7xJjUH0TSUG4gtqlrqlBoY5qs/exec';
   try {
-    // Ambil path dan query string setelah /api
     const targetUrl = APPS_SCRIPT_BASE_URL + req.url.replace('/api', '');
     const response = await fetch(targetUrl);
     const data = await response.text();
@@ -10,4 +9,4 @@ export default async function handler(req, res) {
   } catch (err) {
     res.status(500).json({ success: false, message: 'Proxy error', error: err.toString() });
   }
-} 
+}
